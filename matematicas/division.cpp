c61650073dbb3b31610161dc77eb78303dbe493f
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-15 23:06:39 +0100

    division

---
+++ matematicas/division.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+// Definición del espacio de nombres Matematicas
+namespace Matematicas {//
+// Created by javier san juan on 15/11/2023.
+//
--- matematicas/multiplicacion.cpp
+++ matematicas/multiplicacion.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 
-// Definición del espacio de nombres Matematicas
+
 namespace Matematicas {
-    // Función de multiplicación
+
     double multiplicacion(double a, double b) {
         return a * b;
     }
 }
 int main() {
-    // Uso de las funciones del espacio de nombres Matematicas
-    double num1 = 10.5;
-    double num2 = 5.0; // Cambié num2 a 5.0 para evitar la división por cero en el ejemplo
 
-    // Llamada a la función multiplicacion
+    double num1 = 33;
+    double num2 = 5;
+
     double resultadoMultiplicacion = Matematicas::multiplicacion(num1, num2);
     std::cout << "Multiplicacion: " << num1 << " * " << num2 << " = " << resultadoMultiplicacion << std::endl;
     return 0;
