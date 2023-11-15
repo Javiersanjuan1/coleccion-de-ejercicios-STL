23f6add2ad739238115e03b2d5bc5697e042f0be
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-15 23:05:01 +0100

    multipliacion

--- matematicas/multiplicacion.cpp
+++ matematicas/multiplicacion.cpp
@@ -6,3 +6,12 @@ namespace Matematicas {
     double multiplicacion(double a, double b) {
         return a * b;
     }
+}
+int main() {
+    // Uso de las funciones del espacio de nombres Matematicas
+    double num1 = 10.5;
+    double num2 = 5.0; // Cambié num2 a 5.0 para evitar la división por cero en el ejemplo
+
+    // Llamada a la función multiplicacion
+    double resultadoMultiplicacion = Matematicas::multiplicacion(num1, num2);
+    std::cout << "Multiplicacion: " << num1 << " * " << num2 << " = " << resultadoMultiplicacion << std::endl;
