6ce66a74ec11478be27f4654b0d646427f3a04e9
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-15 23:17:20 +0100

    areatriangulo

--- geometría/areacirculo.cpp
+++ geometría/areacirculo.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
-#include <cmath> // Necesario para la función pow y M_PI
-
+#include <cmath>
 namespace Matematicas {
-    // Función para calcular el área de un círculo
+
     double calcularAreaCirculo(double radio) {
         return M_PI * pow(radio, 2);
     }
 }
 int main() {
-    // Solicitar al usuario el radio del círculo
     std::cout << "Ingrese el radio del circulo: ";
     double radio;
     std::cin >> radio;
 
-    // Verificar que el radio sea no negativo
     if (radio < 0) {
         std::cerr << "Error: El radio no puede ser negativo." << std::endl;
-        return 1; // Código de error
+        return 1;
     }
+    double area = Matematicas::calcularAreaCirculo(radio);
+    std::cout << "El area del circulo con radio " << radio << " es: " << area << std::endl;
+
+    return 0;
+}
---
+++ geometría/areatriangulo.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+// Función para calcular el área del triángulo
+double calcularAreaTriangulo(double base, double altura) {
+    return 0.5 * base * altura;
+}
