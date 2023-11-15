1f20faabb0d0dbcd28eedb3c36ffe07f54017c53
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-15 23:07:56 +0100

    division

--- matematicas/division.cpp
+++ matematicas/division.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 
-// Definición del espacio de nombres Matematicas
+
 namespace Matematicas {//
-    // Función de división
     double division(double a, double b) {
-        // Verificar que el divisor no sea cero antes de realizar la división
+
         if (b != 0) {
             return a / b;
         } else {
-            // Manejo de error si se intenta dividir por cero
             std::cerr << "Error: No se puede dividir por cero." << std::endl;
-            return 0; // Se podría retornar algún valor especial o lanzar una excepción según sea necesario
+            return 0;
         }
     }
 }
 int main() {
-    // Uso de las funciones del espacio de nombres Matematicas
     double num1 = 10.5;
-    double num2 = 5.0; // Cambié num2 a 5.0 para evitar la división por cero en el ejemplo
+    double num2 = 5.0;
+    double resultadoDivision = Matematicas::division(num1, num2);
+    std::cout << "Division: " << num1 << " / " << num2 << " = " << resultadoDivision << std::endl;
 
-// Created by javier san juan on 15/11/2023.
-//
+    return 0;
+}
+
