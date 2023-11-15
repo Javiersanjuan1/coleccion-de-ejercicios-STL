f37e34e886dc5bbbb9de858ccfdbb8f52c6c64a4
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-15 23:14:52 +0100

    areacirculo

--- geometría/areacirculo.cpp
+++ geometría/areacirculo.cpp
@@ -7,3 +7,14 @@ namespace Matematicas {
         return M_PI * pow(radio, 2);
     }
 }
+int main() {
+    // Solicitar al usuario el radio del círculo
+    std::cout << "Ingrese el radio del circulo: ";
+    double radio;
+    std::cin >> radio;
+
+    // Verificar que el radio sea no negativo
+    if (radio < 0) {
+        std::cerr << "Error: El radio no puede ser negativo." << std::endl;
+        return 1; // Código de error
+    }
