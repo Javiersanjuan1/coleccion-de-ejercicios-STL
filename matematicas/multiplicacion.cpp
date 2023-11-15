007ce3c7bca16e23ce8509ed6ff60e4ff38d988e
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-15 23:04:30 +0100

    multipliacion

---
+++ matematicas/multiplicacion.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// Definición del espacio de nombres Matematicas
+namespace Matematicas {
+    // Función de multiplicación
+    double multiplicacion(double a, double b) {
+        return a * b;
+    }
