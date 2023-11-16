7beace3c0d9f5af054a9c4a7cbb8a40296422ba7
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-16 09:41:14 +0100

    gravitacionuniversal

--- Ciencia/fisica/gravitacionuniversal.cpp
+++ Ciencia/fisica/gravitacionuniversal.cpp
@@ -6,3 +6,6 @@ const double G = 6.67430e-11; // en N(m^2)/(kg^2)
 double calcularFuerzaGravitatoria(double masa1, double masa2, double distancia) {
     return G * (masa1 * masa2) / std::pow(distancia, 2);
 }
+int main() {
+    // Ejemplo de uso
+    double masa1, masa2, distancia;
