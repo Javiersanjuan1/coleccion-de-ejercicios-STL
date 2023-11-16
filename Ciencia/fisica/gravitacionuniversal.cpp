9c6ca9bbb9fd800ccd6f43bce63bf5af833746b2
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-16 09:57:39 +0100

    mapsetvector

--- Ciencia/fisica/gravitacionuniversal.cpp
+++ Ciencia/fisica/gravitacionuniversal.cpp
@@ -2,12 +2,10 @@
 #include <cmath>
 const double G = 6.67430e-11; // en N(m^2)/(kg^2)
 
-// Función para calcular la fuerza gravitatoria
 double calcularFuerzaGravitatoria(double masa1, double masa2, double distancia) {
     return G * (masa1 * masa2) / std::pow(distancia, 2);
 }
 int main() {
-    // Ejemplo de uso
     double masa1, masa2, distancia;
     std::cout << "Ingrese la masa del objeto 1 (en kilogramos): ";
     std::cin >> masa1;
