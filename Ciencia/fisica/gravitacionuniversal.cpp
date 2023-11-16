eccbeb072939cc1366ded9cb9a9b23d09a5b099a
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-16 09:41:02 +0100

    gravitacionuniversal

--- Ciencia/fisica/gravitacionuniversal.cpp
+++ Ciencia/fisica/gravitacionuniversal.cpp
@@ -1,3 +1,8 @@
 #include <iostream>
 #include <cmath>
+const double G = 6.67430e-11; // en N(m^2)/(kg^2)
 
+// Función para calcular la fuerza gravitatoria
+double calcularFuerzaGravitatoria(double masa1, double masa2, double distancia) {
+    return G * (masa1 * masa2) / std::pow(distancia, 2);
+}
